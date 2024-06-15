796e719edd1454461f61a53a524d7b48c521617e
Simciutoja <szymon.bludnik@icloud.com>
2024-06-15 18:27:28 +0200

    [E] zmiana autha

--- auth.ts
+++ auth.ts
@@ -3,11 +3,27 @@ import Discord from "next-auth/providers/discord";
 import { SupabaseAdapter } from "@auth/supabase-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Discord],
+  providers: [
+    Discord({
+      profile(profile) {
+        return { role: profile.role ?? "GOŚĆ" };
+      },
+    }),
+  ],
   adapter: SupabaseAdapter({
     url: process.env.SUPABASE_URL,
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
   }),
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    session({ session, token }) {
+      session.user.role = token.role;
+      return session;
+    },
+  },
   events: {
     createUser: async (message) => {
       const response = await fetch(process.env.NEXTJS_PAGE + "/api/auth/mail", {
